eaf28359746bd1277083e3f8d8e5a74169e94517
Ivan Aburto <ivanaburto@MacBook-Pro-de-Ivan.local>
2024-06-03 14:49:30 -0400

    Arrgalndo errores

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "trackordertotal01d20737": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
